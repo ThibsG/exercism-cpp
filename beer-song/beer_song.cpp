7a5cdb44bf3961ca060036add66dd29306413d3f
Thibaud <thibaud@debian>
2015-07-06 23:47:08 +0200

    Simplidying beer-song
     - Remove additional func call
     - Simplify special cases

--- beer-song/beer_song.cpp
+++ beer-song/beer_song.cpp
@@ -7,46 +7,38 @@ using namespace std;
 namespace beer
 {
 
-static string getVerse(int verseNumber);
-
 string verse(int verseNumber)
 {
-  return getVerse(verseNumber);
+  if(verseNumber == 2)
+  {
+    return "2 bottles of beer on the wall, 2 bottles of beer.\n"
+           "Take one down and pass it around, 1 bottle of beer on the wall.\n";
+  }
+  else if(verseNumber == 1)
+  {
+    return "1 bottle of beer on the wall, 1 bottle of beer.\n"
+           "Take it down and pass it around, no more bottles of beer on the wall.\n";
+  }
+  else if(verseNumber <= 0)
+  {
+    return "No more bottles of beer on the wall, no more bottles of beer.\n"
+           "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+  }
+
+  stringstream ss;
+  ss << verseNumber << " bottles" << " of beer on the wall, "
+     << verseNumber << " bottles" << " of beer.\nTake "
+     << "one down and pass it around, " << (verseNumber - 1)
+     << " bottles" << " of beer on the wall.\n";
+
+  return ss.str();
 }
 
 string sing(int start, int end)
 {
   stringstream ss;
   for(auto i = start ; i >= end ; --i)
-    ss << getVerse(i) << (i != end ? "\n" : "");
-  return ss.str();
-}
-
-static string getVerse(int verseNumber)
-{
-  if(verseNumber <= 0)
-  {
-    return "No more bottles of beer on the wall, no more bottles of beer.\n"
-           "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
-  }
-
-  const bool moreThanOneBeer = verseNumber > 1;
-
-  stringstream ss;
-  ss << verseNumber << " bottle" << (moreThanOneBeer?"s":"") << " of beer on the wall, "
-     << verseNumber << " bottle" << (moreThanOneBeer?"s":"") << " of beer.\nTake ";
-
-  if(verseNumber > 1)
-  {
-    const bool moreThanTwoBeer = (verseNumber - 1) > 1;
-    ss << "one down and pass it around, " << (verseNumber - 1)
-       << " bottle" << (moreThanTwoBeer?"s":"") << " of beer on the wall.\n";
-  }
-  else if(verseNumber == 1)
-  {
-    ss << "it down and pass it around, no more bottles of beer on the wall.\n";
-  }
-
+    ss << verse(i) << (i != end ? "\n" : "");
   return ss.str();
 }
 
