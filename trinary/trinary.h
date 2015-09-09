fb621c08f9c42371f932d2b4cbfce71e76d31b20
Thibaud <thibaud@debian>
2015-09-09 23:48:53 +0200

    Trinary: improve using std::accumulate

--- trinary/trinary.h
+++ trinary/trinary.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <algorithm>
-#include <cmath>
 
 namespace trinary
 {
@@ -20,14 +19,9 @@ unsigned int to_decimal(const std::string& input)
   if(std::any_of(input.cbegin(), input.cend(), [](char c){return c > numMax || c < numMin;}))
     return 0;
 
-  auto decimal = 0;
-  auto exp = input.size();
-
-  for(const auto& c : input) {
-    decimal += (c - asciiOffset) * std::pow(3, --exp);
-  }
-
-  return decimal;
+  return std::accumulate(input.cbegin(), input.cend(), 0, [](std::size_t l, char c) {
+    return (c - asciiOffset) + (l * 3);
+  });
 }
 
 }
